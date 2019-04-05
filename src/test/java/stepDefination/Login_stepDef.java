fab8543c3fc75f9b074b1742f30db0733d644d15
gulziraAbudula <gzabudula21@gmail.com>
2019-04-05 17:08:36 -0400

    Login step definitions ( method boyd is empty)

--- src/test/java/stepDefination/Login_stepDef.java
+++ src/test/java/stepDefination/Login_stepDef.java
@@ -1,6 +1,25 @@
 package stepDefination;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class Login_stepDef {
+
+    @Given("^User goes to trello\\.com$")
+    public void user_goes_to_trello_com() throws Throwable {
+
+    }
+
+    @When("^User enters valid username and password$")
+    public void user_enters_valid_username_and_password() throws Throwable {
+
+    }
+
+    @Then("^user should log in successfully$")
+    public void user_should_log_in_successfully() throws Throwable {
+
+    }
 
 
 }
